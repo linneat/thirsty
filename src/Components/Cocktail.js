7280a8915a87bf3adbb6b040ff8b281f2e379cbb
linneat <51674082+linneat@users.noreply.github.com>
2020-04-13 18:10:39 +0200

    api

--- src/Components/Cocktail.js
+++ src/Components/Cocktail.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import CocktailService from "../Services/CocktailService";
+import margarita from './margarita.json';
 
 export default class Cocktail extends Component {
   constructor(props) {
@@ -10,18 +10,90 @@ export default class Cocktail extends Component {
     };
   }
 
+  convertDataToRecipe(data) {
+    const drink = data.drinks[0];
+
+    const ingredientsWithMeasure = [];
+    for(var i = 1; i <= 15; i++) {
+        var ingredientProperty = "strIngredient" + i;
+        var measureProperty = "strMeasure" + i;
+
+        if (drink[ingredientProperty] !== null) {
+            ingredientsWithMeasure.push({
+                name: drink[ingredientProperty],
+                measure: drink[measureProperty]
+            })
+        }
+    }
+
+    return {
+        name: drink.strDrink,
+        thumbnail: drink.strDrinkThumb,
+        instructions: drink.strInstructions.split(". "),
+        ingredients: ingredientsWithMeasure,
+    };
+  }
+
   componentDidMount() {
-    const recipe = CocktailService.getRandomCocktail(
-      this.props.location.mainIngredient
-    );
-    this.setState(() => ({
-      recipe: recipe,
-    }));
+    const useFakeData = false;
+    if (useFakeData) {
+      var recipe = this.convertDataToRecipe(margarita)
+      this.setState({
+        recipe: recipe,
+      });
+      return;
+    }
+
+    if (this.props.location.mainIngredient === undefined) {
+      this.setState({
+        error: "undefined main ingredient",
+      });
+    } else {
+      var url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + this.props.location.mainIngredient
+      fetch(url)
+            .then(response => response.json())
+            .then(
+              (data) => {
+                if (data.drinks.length === 0) {
+                  this.setState({
+                    error: "no cocktails for this main ingredient: " + this.props.location.mainIngredient,
+                  });
+                } else {
+                  var randomDrink = data.drinks[Math.floor(Math.random() * data.drinks.length)];
+                  var url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + randomDrink.idDrink
+
+                  fetch(url)
+                    .then(response => response.json())
+                    .then(
+                      (data) => {
+                        var recipe = this.convertDataToRecipe(data)
+                        this.setState({
+                          recipe: recipe,
+                        });
+                      },
+                      (error) => {
+                        this.setState({
+                          error: error,
+                        });
+                      }
+                    )
+                }
+              },
+              (error) => {
+                this.setState({
+                  error: error,
+                });
+              }
+            )
+    }
   }
 
   render() {
-    if (this.state.recipe.name === undefined) {
-      return <div>loading...</div>;
+    if (this.state.error) {
+      return <div>an error occured: {this.state.error}</div>;
+    }
+    else if (this.state.recipe.name === undefined) {
+      return <div>loading the cocktail...</div>;
     } else {
       return (
         <div>
