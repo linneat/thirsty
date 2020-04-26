c2b848cea7c6194b506d7044f2f25f8ca9b1c990
linneat <51674082+linneat@users.noreply.github.com>
2020-04-26 16:45:31 +0200

    Updates

---
+++ precache-manifest.bc62ed6d9ac0032233c5b7c104d9fbd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e7d2c3042ba0e074d846a91ca726ad",
+    "url": "/thirsty/index.html"
+  },
+  {
+    "revision": "0f086c61482574daec00",
+    "url": "/thirsty/static/css/main.24d494a7.chunk.css"
+  },
+  {
+    "revision": "9bd8b63beb48c6495689",
+    "url": "/thirsty/static/js/2.7521f491.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thirsty/static/js/2.7521f491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f086c61482574daec00",
+    "url": "/thirsty/static/js/main.8e37e8e3.chunk.js"
+  },
+  {
+    "revision": "26b3279949352c78bc83",
+    "url": "/thirsty/static/js/runtime-main.6f377d81.js"
+  }
+]);
